291370a9dcc4c413f5c4503e228cbf7143d614ae
gperez <gperez@tiff.net>
2014-02-21 09:59:39 -0500

    Base version of application

---
+++ App_Code/PhotoUtils.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Web;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Tiff
+{
+    /// <summary>
+    /// Functions to manipulate uploaded images (resizing / cropping)
+    /// </summary>
+    /// 
+
+    public class PhotoUtils
+    {
+
+        /// <summary>
+        /// Function that manages the process of uploading an image file
+        /// </summary>
+        /// <param name="postedfile"></param>
+        /// <param name="targetfilename"></param>
+        /// <param name="folder"></param>
+        /// <param name="maxsize"></param>
+        /// <param name="hmindimension"></param>
+        /// <returns></returns>
+        public String UploadImage(HttpPostedFile postedfile, String targetfilename, String folder, int maxsize = 262144, int hmindimension = 0)
+        {
+            String result = "";
+            // if hmindimension=0 use default settings
+            if (hmindimension == 0)
+            {
+                hmindimension = Convert.ToInt32(Resources.AppSettings.uploadheight);
+            }                     
+            Boolean fileOK = false;
+            int fileSize = postedfile.ContentLength;            
+            String maxk = ((int)((double)maxsize / 1024)).ToString();
+            if  (fileSize > 0 & targetfilename.Length > 0)
+            {
+                String fileExtension = System.IO.Path.GetExtension(postedfile.FileName).ToLower();
+                String[] allowedExtensions = { ".jpg", ".jpeg" };
+                for (int i = 0; i <= allowedExtensions.Length - 1; i++)
+                {
+                    if (allowedExtensions[i].Equals(fileExtension))
+                    {
+                        fileOK = true;
+                    }
+                }
+                if (fileOK)
+                {
+                    if (fileSize < maxsize)
+                    {
+                        try
+                        {
+                            result = ResizeImageUpload(postedfile.InputStream, targetfilename, folder, hmindimension);
+                        }
+                        catch(Exception ex)
+                        {
+                            result = "ERROR: File could not be uploaded <br>" + ex.Message;
+                        }
+                    }
+                    else
+                    {
+                        result = "ERROR: File is larger than " + maxk + "K. Please upload a smaller image";
+                    }
+                }
+                else
+                {
+                    result = "ERROR: Cannot accept files of this type.";
+                }
+            }
+            else
+            {
+                result = "ERROR: Cannot upload photos without valid targetfilename.";
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Function that manages resizing the uploaded image files to a specific target size for standarization
+        /// it scales the images based on values in AppSettings file
+        /// </summary>
+        /// <param name="inputfilestream"></param>
+        /// <param name="finalfilename"></param>
+        /// <param name="folderpath"></param>
+        /// <param name="hmindimension"></param>
+        /// <returns></returns>
+        public String ResizeImageUpload(Stream inputfilestream, String finalfilename, String folderpath, int hmindimension)
+        {
+            String result = "";
+            // enforce final uploaded images to have a fixed height            
+            int newStillWidth, newStillHeight;
+            int ori1;
+            Image originalimg;
+            try
+            {
+                originalimg = System.Drawing.Image.FromStream(inputfilestream);
+                if (originalimg.Width > originalimg.Height)
+                {
+                    // landscape rules
+                    ori1 = originalimg.Height;
+                    newStillHeight = hmindimension;
+                    newStillWidth = (int)((double)originalimg.Width * hmindimension / ori1);
+                }
+                else
+                {
+                    // portrait rules
+                     ori1 = originalimg.Width;
+                    newStillHeight = hmindimension;
+                    newStillWidth = (int)((double)newStillHeight * originalimg.Width / originalimg.Height);
+                }
+                Bitmap still = new Bitmap(newStillWidth, newStillHeight);
+                Graphics gr_dest_still = Graphics.FromImage(still);
+                SolidBrush sb = new SolidBrush(System.Drawing.Color.White);
+                gr_dest_still.FillRectangle(sb, 0, 0, still.Width, still.Height);
+                gr_dest_still.DrawImage(originalimg, 0, 0, still.Width, still.Height);
+                try
+                {
+                    ImageCodecInfo codecencoder = GetEncoder("image/jpeg");
+                    int quality = 90;
+                    EncoderParameters encodeparams = new EncoderParameters(1);
+                    EncoderParameter qualityparam = new EncoderParameter(Encoder.Quality, quality);
+                    encodeparams.Param[0] = qualityparam;
+                    still.SetResolution(96, 96);
+                    if (!folderpath.EndsWith("\\")){
+                        folderpath += "\\";
+                    }
+                    still.Save(folderpath  + finalfilename, codecencoder, encodeparams);
+                    result = "OK: File uploaded!";
+                }
+                catch(Exception ex)
+                {
+                    result = "ERROR: there was a problem saving the image. " + ex.Message;
+                }
+                if (still!=null)
+                {
+                    still.Dispose();                    
+                }    
+            }
+            catch(Exception ex)
+            {
+                result = "ERROR: that was not an image we could process. " + ex.Message;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Function that handles cropping the images after uploading
+        /// images are cropped/scaled to dimensions specified in the AppSettings file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="sourcefolder"></param>
+        /// <param name="targetfolder"></param>
+        /// <param name="imgfolder"></param>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <param name="W"></param>
+        /// <param name="H"></param>
+        /// <returns></returns>
+        public String CropImage(String filename, String sourcefolder, String targetfolder, String imgfolder, int X, int Y, int W, int H)
+        {
+            String result = "";            
+            // enforce final cropped images to have fixed dimensions
+            int croppedfinalw, croppedfinalh;
+            croppedfinalh = Convert.ToInt32(Resources.AppSettings.passheight);
+            croppedfinalw = Convert.ToInt32(Resources.AppSettings.passwidth);
+            try
+            {
+                if (!imgfolder.EndsWith("\\"))
+                {
+                    imgfolder += "\\";
+                }
+                String sourcepath = imgfolder + sourcefolder + "\\";
+                Bitmap image1 = (Bitmap)Image.FromFile(sourcepath + filename, true);
+                Rectangle rect = new Rectangle(X, Y, W, H);
+                Bitmap cropped = image1.Clone(rect, image1.PixelFormat);
+                // dispose original image in case we need to overwrite it below
+                if (image1 != null)
+                {
+                    image1.Dispose();                    
+                }    
+                Bitmap finalcropped= new Bitmap(croppedfinalw, croppedfinalh);
+                Graphics gr_finalcropped  = Graphics.FromImage(finalcropped);
+                SolidBrush sb = new SolidBrush(System.Drawing.Color.White);
+                gr_finalcropped.FillRectangle(sb, 0, 0, finalcropped.Width, finalcropped.Height);
+                gr_finalcropped.DrawImage(cropped, 0, 0, finalcropped.Width, finalcropped.Height);
+                try
+                {
+                    ImageCodecInfo codecencoder  = GetEncoder("image/jpeg");
+                    int quality = 92;
+                    EncoderParameters encodeparams  = new EncoderParameters(1);
+                    EncoderParameter qualityparam = new EncoderParameter(Encoder.Quality, quality);
+                    encodeparams.Param[0] = qualityparam;
+                    finalcropped.SetResolution(240, 240);
+                    sourcepath = sourcepath.Replace(sourcefolder, targetfolder);
+                    finalcropped.Save(sourcepath + filename, codecencoder, encodeparams);
+                    result = "OK - File cropped";
+                }
+                catch(Exception ex)
+                {
+                    result = "ERROR: there was a problem saving the image. " + ex.Message;
+                }
+                if (cropped != null)
+                {
+                    cropped.Dispose();                    
+                }
+                if (finalcropped != null)
+                {
+                    finalcropped.Dispose();                    
+                }
+            }
+            catch(Exception ex)
+            {
+                result = "ERROR: that was not an image we could process. " + ex.Message;
+            }
+            return result;
+        }
+
+        public ImageCodecInfo GetEncoder(String mimetype)
+        {
+            ImageCodecInfo result = null;
+            foreach (ImageCodecInfo codec in ImageCodecInfo.GetImageEncoders())
+            {
+                if (codec.MimeType == mimetype)
+                {
+                    result = codec;
+                }
+            }
+            return result;
+        }
+
+        public int CalculateResizedWidth(String filename, String folderpath, int newh)
+        {
+            int result = 0;
+            if (!folderpath.EndsWith("\\"))
+            {
+                folderpath += "\\";
+            }
+            String fullpath = folderpath + filename;
+            Bitmap image1 = (Bitmap)Image.FromFile(fullpath, true);
+            if (image1 != null)
+            {
+                result = (int)((double)newh * image1.Width / image1.Height);
+                image1.Dispose();
+            }
+            return result;
+        }
+
+    }   
+    
+}
+
+
---
+++ Default.aspx.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Tiff;
+
+public partial class _Default : System.Web.UI.Page
+{
+        
+    int hminRaw = Convert.ToInt32(Resources.AppSettings.uploadheight);
+    int imgUploadPreview = Convert.ToInt32(Resources.AppSettings.uploadpreviewh);
+    int maxSize = Convert.ToInt32(Resources.AppSettings.maxuploadsize);
+    int hminCropped = Convert.ToInt32(Resources.AppSettings.passheight);
+    int wminCropped = Convert.ToInt32(Resources.AppSettings.passwidth);
+    int prevw = Convert.ToInt32(Resources.AppSettings.previewwidth);
+    int prevh = Convert.ToInt32(Resources.AppSettings.previewheight);    
+    PhotoUtils myphotoutils = new PhotoUtils();
+
+    protected void Page_Load(Object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            preview.Width =  new Unit(prevw);
+            preview.Height =  new Unit(prevh);
+            previewdiv.Attributes["style"] = previewdiv.Attributes["style"].Replace("120", prevw.ToString()).Replace("160", prevh.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Handle image upload
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void UploadPhoto(Object sender, EventArgs e)
+    {
+        errorLiteral.Text = "";
+        String path  = Server.MapPath("~/uploaded_images/raw/");        
+        int fileSize = FileUpload1.PostedFile.ContentLength;
+        String testfilename = "image_upload_test.jpg";
+        String resultstr = "";        
+        if (FileUpload1.HasFile)
+        {
+            String opresult = myphotoutils.UploadImage(FileUpload1.PostedFile, testfilename, path, maxSize, hminRaw);
+            resultstr = opresult;
+            if (opresult == "OK: File uploaded!")
+            {
+                imagenameLiteral.Text = testfilename;
+                ViewState["imagename"] = testfilename;
+                Image1.ImageUrl = "~/uploaded_images/raw/" + testfilename ;
+                Image1.Height = new Unit(imgUploadPreview);
+                Image1.Width = myphotoutils.CalculateResizedWidth((String)ViewState["imagename"], path, imgUploadPreview);
+                preview.ImageUrl = "~/uploaded_images/raw/" + testfilename;
+                
+                int jsWidth = myphotoutils.CalculateResizedWidth(ViewState["imagename"].ToString(), path, hminRaw);
+                
+                UpdatePreviewJsWH(jsWidth,hminRaw, path, testfilename);
+                croppedpreviewLiteral.Visible = true;
+            }
+            else
+            {
+                resultstr = opresult;
+                errorLiteral.Text = resultstr;
+                croppedpreviewLiteral.Visible = false;
+            }
+        }
+        else
+        {
+            resultstr = "ERROR: Cannot upload photos without valid image file.";
+            errorLiteral.Text = resultstr;
+        }
+    }
+
+    /// <summary>
+    /// Hanlde image cropping
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void btnCrop_Click(Object sender, EventArgs e)
+    {
+        String path = Server.MapPath("~/uploaded_images/");
+        String sourcefolder = "raw";
+        String targetfolder = "cropped";
+        if (Image1.ImageUrl.Contains("cropped"))
+        {
+            sourcefolder = "cropped";            
+        }
+        String myimagename = ViewState["imagename"].ToString();
+        string opres = myphotoutils.CropImage(myimagename, sourcefolder, targetfolder, path, (int)Double.Parse(X1value.Value), (int)Double.Parse(Y1value.Value), (int)Double.Parse(Wvalue.Value), (int)Double.Parse(Hvalue.Value));
+        if (opres.StartsWith("OK"))
+        {
+            Image1.ImageUrl = "~/uploaded_images/cropped/" + myimagename;
+            Image1.Height = new Unit(imgUploadPreview);
+            Image1.Width = myphotoutils.CalculateResizedWidth(myimagename, Server.MapPath("~/uploaded_images/cropped/"), imgUploadPreview);
+            preview.ImageUrl = "~/uploaded_images/cropped/" + myimagename;
+            // update JS paramaters for refresh function
+            UpdatePreviewJsWH(wminCropped, hminCropped, path.Replace("raw", "cropped"), myimagename);
+        }
+        else
+        {
+            errorLiteral.Text = opres;
+        }
+    }
+
+    /// <summary>
+    /// Handle javascript markup based on the dimensions of the image being cropped
+    /// </summary>
+    /// <param name="neww"></param>
+    /// <param name="newh"></param>
+    /// <param name="folderpath"></param>
+    /// <param name="filename"></param>
+    void UpdatePreviewJsWH(int neww, int newh, String folderpath, String filename)
+    {
+        // change width based on source image
+        int startselect = jcropLiteral.Text.IndexOf("width: Math.round");
+        int startvalue = jcropLiteral.Text.IndexOf("(", startselect);
+        int endvalue = jcropLiteral.Text.IndexOf(")", startvalue);
+        String selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        String newvalue = "(rx*" + neww.ToString() + ")";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);
+        // change height based on source image
+        startselect = jcropLiteral.Text.IndexOf("height: Math.round");
+        startvalue = jcropLiteral.Text.IndexOf("(", startselect);
+        endvalue = jcropLiteral.Text.IndexOf(")", startvalue);
+        selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        newvalue = "(ry*" + newh.ToString() + ")";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);
+        // Configure Explicit Resizing 
+        startselect = jcropLiteral.Text.IndexOf("trueSize:");
+        startvalue = jcropLiteral.Text.IndexOf("[", startselect);
+        endvalue = jcropLiteral.Text.IndexOf("]", startvalue);
+        selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        newvalue = "[" + neww.ToString() + ", " + newh.ToString() + "]";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);  
+        // determine fixed image ratio from Appsettings
+        String ratio  = Resources.AppSettings.passwidth + "/" + Resources.AppSettings.passheight;
+        startselect = jcropLiteral.Text.IndexOf("aspectRatio:");
+        startvalue = jcropLiteral.Text.IndexOf(" ", startselect);
+        endvalue = jcropLiteral.Text.IndexOf(",", startvalue);
+        selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        newvalue = " " + ratio + ",";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);
+        //configure default values for rx     
+        startselect = jcropLiteral.Text.IndexOf("var rx");
+        startvalue = jcropLiteral.Text.IndexOf("=", startselect);
+        endvalue = jcropLiteral.Text.IndexOf(";", startvalue);
+        selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        newvalue = "= " + Resources.AppSettings.previewwidth + " / c.w;";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);
+        //configure default values for ry 
+        startselect = jcropLiteral.Text.IndexOf("var ry ");
+        startvalue = jcropLiteral.Text.IndexOf("=", startselect);
+        endvalue = jcropLiteral.Text.IndexOf(";", startvalue);
+        selectvalue = jcropLiteral.Text.Substring(startvalue, endvalue - startvalue + 1);
+        newvalue = "= " + Resources.AppSettings.previewheight + " / c.h;";
+        jcropLiteral.Text = jcropLiteral.Text.Replace(selectvalue, newvalue);        
+    }
+
+}
